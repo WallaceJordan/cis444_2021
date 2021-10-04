e14306e056e232cb8e8c29b3d028c0628cc6503c
Ubuntu <ubuntu@ip-172-31-11-164.us-west-2.compute.internal>
2021-10-04 22:13:03 +0000

    Began work on server_time.html in templates.

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 USER_PASSWORDS = { "cjardin": "strong password"}
 
 IMGS_URL = {
-            "DEV" : "/static",
-            "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
-            "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
-            }
+        "DEV" : "/static",
+        "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
+        "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
+        }
 
-CUR_ENV = "PRD"
+CUR_ENV = "DEV"
 
 @app.route('/') #endpoint
 def index():
@@ -39,7 +39,7 @@ def backp():
 #Assigment 2
 @app.route('/ss1') #endpoint
 def ss1():
-    return render_template('server_time.html', server_time= str(datetime.datetime.now()) )
+    return render_template('server_time.html', server_time= str(datetime.datetime.now()),img_url=IMGS_URL[CUR_ENV] )
 
 
 app.run(host='0.0.0.0', port=80)
