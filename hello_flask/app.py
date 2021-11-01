1f393f97a7748702c09c36868effe68cf9e00baf
Ubuntu <ubuntu@ip-172-31-11-164.us-west-2.compute.internal>
2021-11-01 03:45:35 +0000

    Successfully handles user logins.

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,36 +1,99 @@
 from flask import Flask,render_template,request
-from flask_json import FlaskJSON, JsonError, json_response, as_json
+from flask_json import FlaskJSON, JsonError, json_response, as_json, jsonify
 import jwt
 
 import datetime
 import bcrypt
-
 
 from db_con import get_db_instance, get_db
 
 app = Flask(__name__)
 FlaskJSON(app)
 
-USER_PASSWORDS = { "cjardin": "strong password"}
-
-IMGS_URL = {
-            "DEV" : "/static",
-            "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
-            "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
-            }
-
-CUR_ENV = "PRD"
 JWT_SECRET = None
-
+JTW_Token = None
 global_db_con = get_db()
-
 
 with open("secret", "r") as f:
     JWT_SECRET = f.read()
 
+def ValidateToken(token):
+    # If the token is still None, it hasn't been set. Don't attempt to
+    # validate.
+    if JWT_Token is None:
+        print("No token stored in server.")
+        return False
+    else:
+        fromServer = jwt.decode(JWT_Token, JWT_SECRET, algorithms=["HS256"])
+        fromRequest = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+
+        if fromServer == fromRequest:
+            print("Valid token.")
+            return True
+        else:
+            print("Tokens do not match.")
+            return False
+
 @app.route('/') #endpoint
-def index():
-    return 'Web App with Python Caprice!' + USER_PASSWORDS['cjardin']
+def assignment3():
+    return render_template("assignment3.html")
+
+@app.route('/login', methods=["POST"]) #endpoint
+def login():
+    print("hi")
+    cur = global_db_con.cursor()
+    try:
+        cur.execute("select username, password from users where username = '" + request.form["username"] + "';")
+    except:
+        return json_response(data={"message":"Error while reading from database."}, status=500)
+
+    row =cur.fetchone()
+    print(row)
+
+    if row is None:
+        print("Username '" + request.form["username"] + "' does not exist.")
+        return json_response(data={"message":("Username '" + request.form["username"] + "' does not exist.")}, status=404)
+    else:
+        print("made it to bcrypt")
+        salted = bcrypt.hashpw(bytes(request.form["password"],  'utf-8' ) , bcrypt.gensalt(12))
+        print(salted)
+        print(bcrypt.checkpw(bytes(row[1], "utf-8"), salted))
+        if bcrypt.checkpw(bytes(row[1], "utf-8"), salted):
+            print(f"'{row[0]}' has logged in.")
+            global JWT_Token
+            JWT_Token = jwt.encode(
+                    {"username": row[0]}, JWT_SECRET, algorithm="HS256")
+            return json_response(data={"jwt": JWT_Token})
+        else:
+            print("Incorrect password.")
+            return json_response(data={"message":"Incorrect password."}, status=404)
+
+
+#@app.route('/login', methods=['POST']) #endpoint
+#def login():
+#    user_name = request.form['username']
+#    print(f"username: {user_name}")
+#    password = request.form['password']
+#    print(f"password: {password}")
+#    cur = global_db_con.cursor()
+#    cur.execute(f"select * from users where username = '{user_name}';")
+#    namecheck = cur.fetchall()
+#    print(namecheck)
+#    dbpw = namecheck[0][2]
+#    salted = bcrypt.hashpw( bytes(password,  'utf-8' ) , bcrypt.gensalt(12))
+#    print(bcrypt.checkpw(bytes(dbpw,'utf-8'), salted ))
+    #print(salted)
+    #print(namecheck)
+#    JWT_Token = jwt.encode({"username" : user_name, "password" : password} , JWT_SECRET, algorithm="HS256")
+#    print(jwt.decode(JWT_Token, JWT_SECRET, algorithms=["HS256"]))
+#    return json_response(jwt=JWT_Token)
+    #return json_response(data=request.form)
+@app.route("/logout") #endpoint
+def logout():
+    global JWT_Token
+    JWT_Token = None
+    print("Logged out.")
+    return json_response(data={"message": "Logged out."})
 
 @app.route('/buy') #endpoint
 def buy():
@@ -65,7 +128,7 @@ def signup():
     password = request.form['password']
     cur = global_db_con.cursor()
     cur.execute(f"select * from users where userID = '{user_name}';")
-    namecheck = cut.fetchone()
+    namecheck = cur.fetchone()
     if nameCheck == None:
         salted = bcrypt.hashpw(bytes(request.form['password'], 'utf-8'), bcrypt.gensalt(12))
         decryptSalt = salted.decode('utf-8')
@@ -115,7 +178,8 @@ def exposejwt():
 def hellodb():
     cur = global_db_con.cursor()
     cur.execute("select * from users;")
-    global_db_con.commit()
+    for r in cur.fetchall():
+        print(r)
     return json_response(status="good")
 
 
--- hello_flask/tools/saltme.py
+++ hello_flask/tools/saltme.py
@@ -1,7 +1,9 @@
 import bcrypt
 
-password_to_salt = "hello"
+nonsalt = input()
+password_to_salt = nonsalt
 salted = bcrypt.hashpw( bytes(password_to_salt,  'utf-8' ) , bcrypt.gensalt(10))
-
+decoded = salted.decode('utf-8')
 print(salted)
+print(decoded)
 
