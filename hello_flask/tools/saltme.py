2fb55e293ae271908a365901a478197ffb06ac7b
Ubuntu <ubuntu@ip-172-31-11-164.us-west-2.compute.internal>
2021-11-21 17:54:36 +0000

    Move files into Assignment3 folder

--- hello_flask/tools/saltme.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-nonsalt = input()
-password_to_salt = nonsalt
-salted = bcrypt.hashpw( bytes(password_to_salt,  'utf-8' ) , bcrypt.gensalt(10))
-decoded = salted.decode('utf-8')
-print(salted)
-print(decoded)
-
